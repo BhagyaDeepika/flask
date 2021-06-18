30add57cf4818ba7ea21f157394ca9d5798bbf84
BhagyaDeepika <47620215+BhagyaDeepika@users.noreply.github.com>
2021-06-18 14:05:54 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,jsonify
-app = Flask(__name__)
-
-@app.route('/home')
-def hello_world():
-   return jsonify({"status_code":200,"msg":'success'})
-
-if __name__ == '__main__':
-   app.run(host="0.0.0.0",port=4800,debug=True)
